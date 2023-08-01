d01684409fa7a4fb802e592e24fd7fb3f25f6dd9
Sean Remenyi <sean.remenyi@gmail.com>
2023-08-01 21:37:11 +0200

    day 1

---
+++ Day-1-Initials/YourInitials.java
@@ -0,0 +1,11 @@
+public class YourInitials {
+    public static void main(String[] args) {
+         System.out.println(" SSS   RRRR ");
+         System.out.println("S   S  R   R");
+         System.out.println("S      R   R");
+         System.out.println(" SSS   RRRR ");
+         System.out.println("    S  R R  ");
+         System.out.println("S   S  R  R ");
+         System.out.println(" SSS   R   R");
+    }
+}
